4db0e2ac4527cbf6fb277e2c264ed1cf81ac9410
schwindt1989 <david89sch@gmail.com>
2024-06-30 18:28:18 -0300

    correccion error en query

--- controllers/recibos-sueldos.controller.js
+++ controllers/recibos-sueldos.controller.js
@@ -8,13 +8,13 @@ async function CreateReciboSueldo(req, res) {
 }
 
 async function GetAllReciboSueldos(req, res) {
-    const sql = 'SELECT rs.id, rs.mes, rs.anio, rs.sueldo, e.nombre, c.nombre as "cateogria" from recibos_sueldos rsleft join empleados e on rs.empleado_id = e.idleft join categorias c on e.categoria_id = c.id';
+    const sql = 'SELECT rs.id, rs.mes, rs.anio, rs.sueldo, e.nombre, c.nombre as "cateogria" from recibos_sueldos rs left join empleados e on rs.empleado_id = e.idleft join categorias c on e.categoria_id = c.id';
     const result = await db.query(sql)
     res.json(result.rows);
 }
 
 async function GetOneReciboSueldo(req, res) {
-    const sql = 'SELECT rs.id, rs.mes, rs.anio, rs.sueldo, e.nombre, c.nombre as "cateogria" from recibos_sueldos rsleft join empleados e on rs.empleado_id = e.idleft join categorias c on e.categoria_id = c.id WHERE rs.id = $1';
+    const sql = 'SELECT rs.id, rs.mes, rs.anio, rs.sueldo, e.nombre, c.nombre as "cateogria" from recibos_sueldos rs left join empleados e on rs.empleado_id = e.idleft join categorias c on e.categoria_id = c.id WHERE rs.id = $1';
     const values = [req.params.id]
     const result = await db.query(sql, values)
     res.json(result.rows[0]);
