f6f3b515ab4dc6261b4928203e3efa467cdaea55
schwindt1989 <david89sch@gmail.com>
2024-06-30 18:00:40 -0300

    correccion error de tipeo

--- controllers/recibos-sueldos.controller.js
+++ controllers/recibos-sueldos.controller.js
@@ -2,7 +2,7 @@ const db = require('../configs/database')
 
 async function CreateReciboSueldo(req, res) {
     const sql = 'INSERT INTO recibos_sueldos (mes, anio, sueldo, empleado_id) VALUES($1, $2, $3, $4) RETURNING *';
-    const values = [req.body.mes, req.body.anio, req.body.suedo, req.body.empleado_id]   
+    const values = [req.body.mes, req.body.anio, req.body.sueldo, req.body.empleado_id]   
     const result = await db.query(sql, values)
     res.json(result.rows[0]);
 }
